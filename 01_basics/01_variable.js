9976267df31d398b6d64d59418fbe0e4c447df1f
yashpandey347683-boop <yash.pandey347683@gmail.com>
2026-08-26 07:28:54 +0000

    it is a container

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,21 @@
+const accountId = 144553
+let accountEmail="yash.pandey347683@gmail.com"
+var accountPassword = "347683"
+accountCity = "Kanpur"
+let accountState;
+
+//accountId = 2  //not allowed
+
+accountEmail= "hcverma@gmail.com"
+accountPassword="47948646"
+accountCity="chennai"
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+
+/*
+it is also a comment
+Prefer not to use var generally use const and let
+because of issue in block scope and functional scope
+*/
---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,16 @@
+"use strict";//treat all JS code as newer version
+
+//alert(3+3)//we are usind nodejs not browser
+
+console.log(3+3) console.log("yash")//it gives error
+
+
+console.log(3+3); console.log("yash")//it is run but it is not to good programmer
+
+
+console.log(3+
+    3);//it is run but it is not good in readiability
+
+
+ console.log("yash")
+
