0540a083e7493bb98405c0687c7a11ffcfeaa68d
yashpandey347683-boop <yash.pandey347683@gmail.com>
2026-08-27 08:19:43 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,37 @@
+"use strict";//treat all JS code as newer version
+
+//alert(3+3)//we are usind nodejs not browser
+
+
+
+
+console.log(3+3); console.log("yash")//it is run but it is not to good programmer
+
+
+console.log(3+
+    3);//it is run but it is not good in readiability
+
+
+ console.log("yash")
+
+
+ let name = "Yash" //string
+ let age = 18  //number(integer,float)
+ let isLoggedIn =  false  //boolean
+
+
+ //DATA TYPES IN JAVASCRIPT(primitive data type)
+
+ //range number=> 2 to power 53
+ //bigint(use in treding,training)
+ //string=>""
+ //bollean=>true/false
+ //null=>standalone value(empty)
+ //undefined(in this value not assigned but variable taken)
+ //symbol=>unique
+ console.log(typeof "yash");
+
+ console.log(typeof null);
+ console.log(typeof undefined);
+
+
