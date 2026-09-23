dfd3d27c12d39789e731bc47ef5625680da5cc6d
yashpandey347683-boop <yash.pandey347683@gmail.com>
2026-09-23 02:31:43 +0000

    datatypes

---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,21 @@
+//PRIMITIVE
+
+//7  Types : String,Numbers,Null,Boolean,undefined,symbol,BigInt
+
+const score = 100
+const score = 103.3
+
+const IsLoggedIn = false
+const outsidetemp = null
+let useremail;
+console.log(useremail);
+
+const id = Symbol('123')
+const anotherid = Symbol('123')
+
+console.log(id==anotherid);
+console.log(id===anotherid);
+
+//reference or non-primitive
+
+//Arrays, //Objects,Functions
